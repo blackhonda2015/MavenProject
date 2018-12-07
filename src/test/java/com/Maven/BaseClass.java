dc2615df492c5c43c06b384a2e87feef2d107e2e
blackhonda2015 <blackhonda2015@gmail.com>
2018-12-06 21:56:10 -0500

    initial commit

---
+++ src/test/java/com/Maven/BaseClass.java
@@ -0,0 +1,25 @@
+package com.Maven;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.annotations.Test;
+
+
+public class BaseClass {
+	
+	WebDriver driver;
+	@Test
+	public void setUp() {
+//	System.setProperty("webdriver.chrome.driver", "src/Drivers/chromedriver.exe");
+//	driver = new ChromeDriver();
+		driver = new HtmlUnitDriver();
+		//driver.manage().window().fullscreen();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+
+}
